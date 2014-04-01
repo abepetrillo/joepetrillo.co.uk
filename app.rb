012b73fce3e43dbb569d1d13442c7f87819553cc
Abe Petrillo <abe.petrillo@gmail.com>
2014-04-01 22:07:23 +0100

    take away login actions

--- app.rb
+++ app.rb
@@ -39,16 +39,6 @@ get '/contact' do
   erb :contact
 end
 
-get '/login/form' do 
-  erb :login_form
-end
-
-post '/login/attempt' do
-  session[:identity] = params['username']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from 
-end
-
 get '/logout' do
   session.delete(:identity)
   erb "<div class='alert alert-message'>Logged out</div>"
